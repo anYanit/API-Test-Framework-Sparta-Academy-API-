add7f07d0ba8fe994f003f7bb266ddbcb8063aed
Anthony Yanit <anyanit1@gmail.com>
2026-05-11 14:54:13 +0100

    add ApiBuilder with a RequestSpecBuilder and RequestSpecification for authentificationLogin

---
+++ src/test/java/com/sparta/utils/ApiBuilder.java
@@ -0,0 +1,32 @@
+package com.sparta.utils;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+
+import java.util.Map;
+import java.util.ResourceBundle;
+
+public class ApiBuilder {
+
+    public static final ResourceBundle resource = ResourceBundle.getBundle("config");
+
+    // base URI
+    public static final String BASE_URI = resource.getString("automation.base_url");
+
+    // POST endpoints
+    public static final String POST_LOGIN = resource.getString("automation.post_login");
+
+    private static RequestSpecBuilder getBaseSpecBuilder(String path) {
+        return new RequestSpecBuilder()
+                .setBaseUri(BASE_URI)
+                .setBasePath(path)
+                .addHeaders(Map.of(
+                        "Accept", "*/*"
+                ));
+    }
+
+    private static RequestSpecification authentificationLogin() {
+        return getBaseSpecBuilder(POST_LOGIN)
+                .build();
+    }
+}
